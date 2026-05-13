32bf174f5c5cd1bfd64fc11c53e5e0c276a17ba4
aviTD27 <avi.doorga27@gmail.com>
2026-05-13 15:55:00 +0200

    Fix Folder Name

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import messy_project_hakeem.src.calculator as calculator
+import messy_project_hakeem.src.helpers.helper as helper
+
+print("starting app")
+
+x = input("Enter your name: ")
+print("Hello " + x)
+
+result = calculator.add(5, 3)
+print("Result:", result)
+
+data = helper.getData()
+print("Data:", data)
